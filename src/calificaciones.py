aeef409e8e89f0a1727f7bbbbd84ec3d82b2c1eb
Carlos Javier Cruz Ramírez <carcruram@alum.us.es>
2022-10-04 17:44:13 +0200

    nuevos cambios

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -26,9 +26,6 @@ def lee_datos():
     cuestionarios = (cuest_1, cuest_2, cuest_3, cuest_4, cuest_5, cuest_6)
     return(parciales, proyectos, cuestionarios)
 
-
-#parciales, proyectos, cuestionarios = lee_datos() #Esto hay que ponerlo en el fichero que tiene que coger los datos
-
 def calcula_nota_cuatrimestre(parciales, proyectos, cuestionarios):
     if proyectos[0]<5:
         calcula_nota_cuatrimestre = 3.0
@@ -37,8 +34,6 @@ def calcula_nota_cuatrimestre(parciales, proyectos, cuestionarios):
         calcula_nota_cuatrimestre = 0.6*parciales[0]+0.1*proyectos[0]+0.1*sum(cuestionarios[:3])
         return(calcula_nota_cuatrimestre)
 
-#print(calcula_nota_cuatrimestre(parciales, proyectos, cuestionarios)) #Esto también
-
 def calcula_nota_cuatrimestre2(parciales, proyectos, cuestionarios):
     if proyectos[1]<5:
         calcula_nota_cuatrimestre2 = 3.0
@@ -46,8 +41,6 @@ def calcula_nota_cuatrimestre2(parciales, proyectos, cuestionarios):
     else:
         calcula_nota_cuatrimestre2 = 0.6*parciales[1]+0.1*proyectos[1]+0.1*sum(cuestionarios[3:])
         return(calcula_nota_cuatrimestre2)
-
-#print(calcula_nota_cuatrimestre2(parciales, proyectos, cuestionarios))
 
 def calcula_nota_evaluacion_continua(parciales, proyectos, cuestionarios):
     if proyectos[0]<5 or proyectos[1]<5:
@@ -61,5 +54,3 @@ def calcula_nota_evaluacion_continua(parciales, proyectos, cuestionarios):
         else:
             nota_evaluacion_continua = (nota_1_cuatrimestre+nota_2_cuatrimestre)/2
     return (nota_evaluacion_continua)
-
-#print(calcula_nota_evaluacion_continua(parciales, proyectos, cuestionarios))
